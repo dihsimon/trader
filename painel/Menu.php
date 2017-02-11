b9a4f29c2bf96381870544d90ca239443a2e7db1
djdiego <diegosimoncarmona@gmail.com>
2017-02-11 13:01:13 -0200

    a

--- painel/Menu.php
+++ painel/Menu.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
+}
 require_once("../util/ValidaLogin.php");
 require_once ("../model/Clientes.php");
 $cliente = new Clientes();
